92d84fdec8f62508dbe308454b55903312697b90
Chunqing Shan <me@vx.st>
2024-05-20 16:57:20 +0000

    Refactor fma8 function to use access_byte helper function

--- fma8.cu
+++ fma8.cu
@@ -16,15 +16,20 @@
 
 #include <cuda_runtime.h>
 
+
+__device__ __forceinline__ int access_byte(int* __restrict__ data, int i) {
+    return data[i>>2] >> ((i&3)<<3) & 0xff;
+}
+
 // Do a 4 8bit fma, r[i] = a[i] * b[i] + c[i]
-__device__ int fma8(int a, int b, int c, int* __restrict__ acore, int* __restrict__ mcore) {
-    int r = 0;
+__device__ __forceinline__ int fma8v4(int a, int b, int c, int* __restrict__ acore, int* __restrict__ mcore) {
+    int res = 0;
 #pragma unroll
     for(int i = 0; i < 4; i++) {
         int a0 = (a >> (i * 8)) & 0xff;
         int b0 = (b >> (i * 8)) & 0xff;
         int c0 = (c >> (i * 8)) & 0xff;
-        int m = mcore[((a0&0x7f)<<7) + (b0&0x7f)];
+        int m = access_byte(mcore, ((a0&0x7f)<<7) + (b0&0x7f));
         int r = 0;
         m |= (a0&0x80) ^ (b0&0x80);
         if((m&0x80)^(c0&0x80)) {
@@ -33,12 +38,29 @@ __device__ int fma8(int a, int b, int c, int* __restrict__ acore, int* __restric
             }else if(m&0x80){
                 // c0 - m
                 if((c0&0x7f) > (m&0x7f)){
-                    r = acore[((c0&0x7f) << 7) + (m&0x7f)];
+                    r = access_byte(acore, ((c0&0x7f) << 7) + (m&0x7f));
                 }else{
-                    r = acore[((m&0x7f) << 7) + (c0&0x7f)];
+                    r = access_byte(acore, ((m&0x7f) << 7) + (c0&0x7f));
+                    r ^= 0x80;
+                }
+            }else{
+                // m - c0
+                if((m&0x7f) > (c0&0x7f)){
+                    r = access_byte(acore, ((m&0x7f) << 7) + (c0&0x7f));
+                }else{
+                    r = access_byte(acore, ((c0&0x7f) << 7) + (m&0x7f));
                     r ^= 0x80;
                 }
             }
+        } else {
+            if((m&0x7f) <= (c0&0x7f)){
+                r = access_byte(acore, ((m&0x7f) << 7) + (c0&0x7f));
+            }else{
+                r = access_byte(acore, ((c0&0x7f) << 7) + (m&0x7f));
+            }
+            r |= m & 0x80;
         }
+        res |= r << (i * 8);
     }
+    return res;
 }
