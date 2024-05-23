40f5ebe6085de9efdac349d2d16b0613a77642ba
Chunqing Shan <me@vx.st>
2024-05-23 13:25:38 +0000

    Copy data asynchronously

--- matmulf8.cu
+++ matmulf8.cu
@@ -21,28 +21,32 @@
 
 float matmul(int* A, int* B, int* C, int n, int m, int p, int* acore, int* mcore) {
     int* d_A, *d_B, *d_C, *d_acore, *d_mcore;
+    // MallocAsync is not supported in older device
     cudaMalloc(&d_A, n * m / 4 * sizeof(int));
     cudaMalloc(&d_B, m * p / 4 * sizeof(int));
     cudaMalloc(&d_C, n * p / 4 * sizeof(int));
     cudaMalloc(&d_acore, 16384);
     cudaMalloc(&d_mcore, 16384);
-    cudaMemcpy(d_A, A, n * m * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, m * p * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_acore, acore, 16384, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_mcore, mcore, 16384, cudaMemcpyHostToDevice);
+
+    cudaStream_t stream;
+    cudaStreamCreate(&stream);
+    cudaMemcpyAsync(d_A, A, n * m * sizeof(int), cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(d_B, B, m * p * sizeof(int), cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(d_acore, acore, 16384, cudaMemcpyHostToDevice, stream);
+    cudaMemcpyAsync(d_mcore, mcore, 16384, cudaMemcpyHostToDevice, stream);
 
     float t;
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
-    cudaEventRecord(start);
+    cudaEventRecord(start, stream);
     // Each kernel thread do 4xf8 result
-    matmulf8<<<dim3(n / 32, p / 32), dim3(32, 8)>>>(d_A, d_B, d_C, n, m, p, d_acore, d_mcore);
-    cudaEventRecord(stop);
+    matmulf8<<<dim3(n / 32, p / 32), dim3(32, 8), 0, stream>>>(d_A, d_B, d_C, n, m, p, d_acore, d_mcore);
+    cudaEventRecord(stop, stream);
     cudaEventSynchronize(stop);
     cudaEventElapsedTime(&t, start, stop);
 
-    cudaMemcpy(C, d_C, n * p * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpyAsync(C, d_C, n * p * sizeof(int), cudaMemcpyDeviceToHost, stream);
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
